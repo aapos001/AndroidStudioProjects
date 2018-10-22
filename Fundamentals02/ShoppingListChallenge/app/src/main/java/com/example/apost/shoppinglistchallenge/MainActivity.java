480a7f56bf09ef0a6a468aa6bc2308c98773ccae
Andrew Apostol <ApostAndrew@gmail.com>
2018-10-22 11:11:08 -0700

    add button to search for store on google maps

---
+++ Fundamentals02/ShoppingListChallenge/app/src/main/java/com/example/apost/shoppinglistchallenge/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.apost.shoppinglistchallenge;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText storeName;
+    private TextView List1, List2, List3, List4, List5, List6, List7, List8, List9;
+    private int listsCounter = 0;
+    public static final int GROCERY_REQUEST = 1;
+
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String grocery = intent.getStringExtra(SecondActivity.EXTRA_GROCERY);
+        List1 = findViewById(R.id.List1);
+        List2 = findViewById(R.id.List2);
+        List3 = findViewById(R.id.List3);
+        List4 = findViewById(R.id.List4);
+        List5 = findViewById(R.id.List5);
+        List6 = findViewById(R.id.List6);
+        List7 = findViewById(R.id.List7);
+        List8 = findViewById(R.id.List8);
+        List9 = findViewById(R.id.List9);
+        storeName = findViewById(R.id.edit_store);
+    }
+
+    public void launchAddItem(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, GROCERY_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GROCERY_REQUEST) {
+            if(resultCode == RESULT_OK) {
+                String grocery = data.getStringExtra(SecondActivity.EXTRA_GROCERY);
+                switch(listsCounter) {
+                    case 0:
+                        List1.setText(grocery);
+                        List1.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 1:
+                        List2.setText(grocery);
+                        List2.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 2:
+                        List3.setText(grocery);
+                        List3.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 3:
+                        List4.setText(grocery);
+                        List4.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 4:
+                        List5.setText(grocery);
+                        List5.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 5:
+                        List6.setText(grocery);
+                        List6.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 6:
+                        List7.setText(grocery);
+                        List7.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 7:
+                        List8.setText(grocery);
+                        List8.setVisibility(View.VISIBLE);
+                        listsCounter++;
+                        break;
+
+                    case 8:
+                        List9.setText(grocery);
+                        List9.setVisibility(View.VISIBLE);
+                        listsCounter = 0;
+                        break;
+                }
+            }
+        }
+    }
+
+    public void SearchStore(View view) {
+        String loc = storeName.getText().toString();
+        Uri uri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
+    }
+}
---
+++ Fundamentals02/ShoppingListChallenge/app/src/main/java/com/example/apost/shoppinglistchallenge/SecondActivity.java
@@ -0,0 +1,49 @@
+package com.example.apost.shoppinglistchallenge;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_GROCERY = "com.example.apost.shoppinglistchallenge.extra.MESSAGE";
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+    public void AddGrocery(View view) {
+        Intent intent = new Intent();
+
+        switch(view.getId()) {
+            case R.id.button_fruit:
+                intent.putExtra(EXTRA_GROCERY, "Fruit");
+                break;
+
+            case R.id.button_milk:
+                intent.putExtra(EXTRA_GROCERY, "Milk");
+                break;
+
+            case R.id.button_cereal:
+                intent.putExtra(EXTRA_GROCERY, "Cereal");
+                break;
+
+            case R.id.button_eggs:
+                intent.putExtra(EXTRA_GROCERY, "Eggs");
+                break;
+
+            case R.id.button_bread:
+                intent.putExtra(EXTRA_GROCERY, "Bread");
+                break;
+        }
+
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
